56bc234f3f1ed825d59e4972a6b5c26aad57549d
jeanne Champion <jeanne.champion@cpe.fr>
2022-12-06 08:54:44 +0100

    ok

--- LibS.py
+++ LibS.py
@@ -1,2 +1,11 @@
 
+from tkinter import Tk , Label , Button, StringVar, Entry, Canvas
 
+Mafenetre=Tk()
+Mafenetre.title("Katz invader")
+Mafenetre.geometry("1000x800")
+BoutonQuitter= Button(Mafenetre, text='quitter',command=Mafenetre.destroy)
+BoutonQuitter.pack(side='right', padx=5, pady=5)
+Canevas = Canvas(Mafenetre, width=700, height=900, bg='blue')
+Canevas.pack(padx=5,pady=5)
+Mafenetre.mainloop()
